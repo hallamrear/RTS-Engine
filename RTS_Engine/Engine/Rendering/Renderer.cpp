99c855959f5f6f30e25e7ab503c603971ca01db7
Hallam Rear <hallamrear@gmail.com>
2023-11-06 19:02:50 +0000

    Changed clear colour.

--- RTS_Engine/Engine/Rendering/Renderer.cpp
+++ RTS_Engine/Engine/Rendering/Renderer.cpp
@@ -146,8 +146,8 @@ namespace Bennett
 	void Renderer::BeginRenderPass()
 	{
 		std::array<VkClearValue, 2> clearValues{};
-		clearValues[0].color = { {0.245f, 0.245f, 0.245f, 1.0f} };
-		clearValues[0].color = { {0.0f, 0.0f, 0.0f, 1.0f} };
+		clearValues[0].color = { { 0.245f, 0.245f, 0.245f, 1.0f } };
+		clearValues[0].color = { { 0.21568627450980393f, 0.25882352941176473f, 0.34901960784313724f, 1.0f } };
 		clearValues[1].depthStencil = { 1.0f, 0 };
 
 		VkRenderPassBeginInfo renderPassInfo{};
